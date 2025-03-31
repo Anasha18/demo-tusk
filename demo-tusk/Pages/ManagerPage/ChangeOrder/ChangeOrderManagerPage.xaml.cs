8cf8a9bb17f49da9a39f6b1eec155543c4d9dba1
wex716 <nikitabarkovskij44@gmail.com>
2025-03-31 18:22:41 +0300

    add logic changeOrder

---
+++ demo-tusk/Pages/ManagerPage/ChangeOrder/ChangeOrderManagerPage.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using demo_tusk.Connection;
+using demo_tusk.Model;
+using demo_tusk.Pages.ManagerPage.MainMenu;
+
+namespace demo_tusk.Pages.ManagerPage
+{
+    /// <summary>
+    /// Логика взаимодействия для ChangeOrderManagerPage.xaml
+    /// </summary>
+    public partial class ChangeOrderManagerPage : Page
+    {
+        private Order _currentOrder;
+
+        public ChangeOrderManagerPage(Order order)
+        {
+            InitializeComponent();
+            _currentOrder = order;
+
+            ComboBoxStatus.ItemsSource = DbConnector.conn.Statuses.ToList();
+            ComboBoxStatus.DisplayMemberPath = "Name";
+
+            LoadOrder();
+        }
+
+        private void LoadOrder()
+        {
+            textBoxId.Text = _currentOrder.Id.ToString();
+            ComboBoxStatus.Text = _currentOrder.Status.Name;
+            textBoxDateTime.Text = _currentOrder.FinishOrder.ToString("yyyy-MM-dd");
+            textBoxCost.Text = _currentOrder.Cost.ToString();
+        }
+
+
+        private void buttonBackPage_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.Frame.Navigate(new ManagerMainMenuPage());
+        }
+
+        private void buttonChangeOrderClick_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBoxCost.Text.Length == 0)
+            {
+                MessageBox.Show(
+                    "Ошибка. Цена доставки заказа не может быть пустой"
+                );
+                return;
+            }
+
+            if (textBoxDateTime.Text.Length == 0)
+            {
+                MessageBox.Show(
+                    "Ошибка. Дата доставки заказа не может быть пустой"
+                );
+                return;
+            }
+
+            if (ComboBoxStatus.Text.Length == 0)
+            {
+                MessageBox.Show(
+                    "Ошибка. Статус заказа не может быть пустым"
+                );
+                return;
+            }
+
+            try
+            {
+                _currentOrder.Status = (Status)ComboBoxStatus.SelectedItem;
+                _currentOrder.FinishOrder = DateTime.Parse(textBoxDateTime.Text);
+                _currentOrder.Cost = decimal.Parse(textBoxCost.Text);
+
+                DbConnector
+                    .conn
+                    .SaveChanges();
+
+                MessageBox.Show(
+                    "Обновление данных успешно произошло!"
+                );
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(
+                    "Ошибка при работе с БД " + exception.Message
+                );
+            }
+        }
+    }
+}
---
+++ demo-tusk/Pages/ManagerPage/MainMenu/ManagerMainMenuPage.xaml.cs
@@ -0,0 +1,131 @@
+﻿using demo_tusk.Connection;
+using demo_tusk.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using demo_tusk.Pages.ManagerPage.CreateClient;
+using demo_tusk.Pages.ManagerPage.CreateOrder;
+using demo_tusk.Pages.ManagerPage.MoreDetailsOrder;
+
+namespace demo_tusk.Pages.ManagerPage.MainMenu
+{
+    /// <summary>
+    /// Логика взаимодействия для ManagerMainMenuPage.xaml
+    /// </summary>
+    public partial class ManagerMainMenuPage : Page
+    {
+        public ManagerMainMenuPage()
+        {
+            InitializeComponent();
+
+            dataGridManager.ItemsSource = DbConnector
+                .conn
+                .Orders
+                .Include(s => s.Status)
+                .Include(cl => cl.ClientFrom)
+                .ToList();
+            
+            
+        }
+
+
+        private void ButtonMoreDetailsOrder_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.Frame.Navigate(new MoreDetailsOrderPage());
+        }
+
+        private void ButtonChangeOrder_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxId.Text))
+            {
+                MessageBox.Show(
+                    "Ошибка. Для изменения данных заказа поле не должно быть пустым, введите id"
+                );
+                return;
+            }
+
+            if (!int.TryParse(textBoxId.Text, out int id))
+            {
+                MessageBox.Show(
+                    "Ошибка. ID заказа должен быть числом"
+                );
+                return;
+            }
+
+            Order? selectedOrder = null;
+            try
+            {
+                selectedOrder =
+                    DbConnector
+                        .conn
+                        .Orders
+                        .FirstOrDefault(o => o.Id == id);
+            }
+            catch
+            {
+                MessageBox.Show(
+                    "Ошибка при работе с БД"
+                );
+                return;
+            }
+
+            if (selectedOrder == null)
+            {
+                MessageBox.Show(
+                    "Такого заказа нет"
+                );
+                return;
+            }
+
+            GlobalVariables.Frame.Navigate(new ChangeOrderManagerPage(selectedOrder));
+        }
+
+        private void buttonCreateOrder_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.Frame.Navigate(new CreateOrderPage());
+        }
+
+        private void buttonCreateClient_Click(object sender, RoutedEventArgs e)
+        {
+            GlobalVariables.Frame.Navigate(new CreateClientPage());
+        }
+
+        private void buttonSelectOrderOnPhoneNumber_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                dataGridManager.ItemsSource =
+                    DbConnector
+                        .conn
+                        .Orders
+                        .Where(order => order.ClientFrom.PhoneNumber == textBoxInputPhoneNumber.Text)
+                        .ToList();
+
+                textBoxInputPhoneNumber.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(
+                    "Ошибка при работе с БД "
+                    + ex.Message);
+            }
+        }
+
+        private void buttonBackToMenu_Click(object sender, RoutedEventArgs routedEventArgs)
+        {
+            GlobalVariables.Frame.Navigate(new AutentificationPage());
+        }
+    }
+}
